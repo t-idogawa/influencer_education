ea4cd77605a2f83f092c39d5715f15cab640d015
yamataso <yamataso0876@gmail.com>
2023-08-30 01:40:07 +0900

    マイグレーションファイルの追加です。

---
+++ database/migrations/2023_08_23_224757_crate_delivery_times_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_times', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('curriculums_id');
+            $table->dateTime('delivery_from', $precision = 0);
+            $table->dateTime('delivery_to', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_times');
+    }
+};
