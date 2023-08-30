97cb1e972924a0aae983618f1dbee64e5ed28904
Tomoki Idogawa <cc.tomo41@gmail.com>
2023-08-30 22:30:10 +0900

    users,curricurum_progressのマイグレーションファイル作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function curriculum_progress() {
+        return $this->hasMany(CurriculumProgress::class);
+    }
+
 }
