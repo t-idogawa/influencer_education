a98d214daf6bc578ba6c6788d9f6aa436a937699
Tomoki Idogawa <cc.tomo41@gmail.com>
2023-09-11 15:00:59 +0900

    ユーザーログイン/新規登録作成

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.headerless')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="">
+                <div class="" style="text-align: right;">
+                  <a href="{{ route('login') }}" class="">ログインはこちら</a>
+                </div>
+                <div class="text-center my-3 fs-1">新規会員登録</div>
 
-                <div class="card-body">
+                <div class="">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -24,9 +27,22 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="row mb-3">
+                            <label for="name_kana" class="col-md-4 col-form-label text-md-end">{{ __('Name_Kana')}}</label>
+
+                            <div class="col-md-6">
+                                <input id="name_kana" type="text" class="form-control @error('name_kana') is-invalid @enderror" name="name_kana" value="{{ old('name_kana') }}" required autocomplete="name_kana" autofocus>
+
+                                @error('name_kana')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -61,12 +77,15 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
+                        <!-- ユーザー側に表示せずに初期値をDBに登録 -->
+                        <input type="hidden" name="profile_image" value="">
+                        <input type="hidden" name="classes_id" value="1">
+
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                <button type="submit" class="btn btn-lg" style="background-color: #e25407; color: white; font-size: 20px;">
+                                  {{ __('Register') }}
                                 </button>
-                            </div>
+                            </div> 
                         </div>
                     </form>
                 </div>
