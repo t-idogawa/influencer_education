7b3549c09859ac1cd0b50cabcb57cdecc5762eec
Tomoki Idogawa <cc.tomo41@gmail.com>
2023-10-01 13:26:17 +0900

    ユーザートップ/共通ヘッダー

--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -12,17 +12,19 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
+    
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- css -->
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div id="">
+        <nav class="navbar navbar-expand-md navbar-light custom-header shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -30,7 +32,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <div>
+                        <a class="btn btn-header" href="">時間割</a>
+                        <a class="btn btn-header" href="">授業進捗</a>
+                        <a class="btn btn-header" href="">プロフィール編集</a> 
+                        </div>
+                        <!-- マージ後にリンク先記載 -->
                     </ul>
 
                     <!-- Right Side Of Navbar -->
